7b26a9703ec90fab9ba0de3ef13bfd11e21ae7e5
cchms <cchms@163.com>
2021-10-28 16:57:32 +0800

    Raspberry pi 4 的第一课传感器的C代码与Python 代码

---
+++ 01led.py
@@ -0,0 +1,25 @@
+"""
+### 如何你使用了源代码，请保留作者名字###
+wiringpi for python3
+编辑：蔡春海
+LED灯模块实验
+LED灯工作只要有高电平或是低电平来驱动工作就可以
+"""
+# 导入wiringpi库
+from wiringpi import *
+
+# 设置全局接口模式为wPi模式
+#wiringPiSetup()
+
+# 设置全局接口模式为BCM模式
+wiringPiSetupGpio()
+
+# 设置当前使用的接口为输出模式
+pinMode(7,OUTPUT)
+
+# 编写闪烁灯
+while(1):
+    digitalWrite(7,1) #点亮7号针脚上的LED灯
+    delay(500) #延时500毫秒
+    digitalWrite(7,0) #关闭7号针脚上的LED灯
+    delay(500)
